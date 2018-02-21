bd61127c8e623a434b1211597bbba0305f9a1f55
Felipe Francischetto <felipefrancischetto@hotmail.com>
2018-02-21 10:48:27 -0300

    feature: creating the controller todo.

---
+++ controllers/todoController.js
@@ -0,0 +1,46 @@
+import Todo from '../db/models/todo';
+import { prop } from 'ramda';
+
+export const getAll = async(req, res, next) => {
+  try{
+    const todos = await Todo.find(req.query)
+    res.json(todos)
+  } catch(err) {
+    return err
+  } 
+}
+
+export const getTodoById = async(req, res, next) => {
+  try{
+    const _id = prop('id', req.params)
+    const todo = await Todo.findById(_id)
+    res.json(todo)
+  } catch(err) {
+    next(err)
+  }
+}
+
+export const saveTodo = async(req, res, next)=> {
+  try{
+    const todo = prop('body', req)
+    const todoModel = new Todo(todo)
+    const newTodo = await todoModel.save()
+    res.json(newTodo)
+  } catch(err) {
+    next(err)
+  }
+}
+
+export const updateTodo = async(req, res, next) => {
+  try{
+    const todo = prop('body', req)
+    const _id = prop('id', req.params)
+    const todoUpdate = await Todo
+      .findByIdAndUpdate(_id, todo, {
+        runValidators: true
+      })
+    res.json(todoUpdate)
+  } catch {
+    next(err)
+  }
+}
