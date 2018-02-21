93d8a725686392b83383cb1f31d88faced35ae48
Felipe Francischetto <felipefrancischetto@hotmail.com>
2018-02-21 14:26:09 -0300

    refactor: removed import and fix indent.

--- db/models/todo.js
+++ db/models/todo.js
@@ -1,10 +1,20 @@
-import { Schema, Mongoose } from 'mongoose';
+import { Schema } from 'mongoose';
 import dbConnection from '../../db/db.connection';
 
 const todoSchema = new Schema({
-  description: { type: String, required: true },
-  done: { type: Boolean, required: true, default: false },
-  createdAt: { type: Date, default: Date.now }
-});
+  description: { 
+    type: String,
+    required: true
+  },
+  done: { 
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+}, { versionKey: false });
 
 export default dbConnection.model('todo', todoSchema);
